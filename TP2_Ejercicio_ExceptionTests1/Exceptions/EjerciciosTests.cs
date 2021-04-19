e83dcf67fc8d808ff28294b9e66bd7380898de29
Isaca1996 <isaacedmundo.hernandez@gmail.com>
2021-04-19 11:42:00 -0300

    ejercicio completo con unit test

---
+++ TP2_Ejercicio_ExceptionTests1/Exceptions/EjerciciosTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TP2_Ejercicio_Exception.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP2_Ejercicio_Exception.Exceptions.Tests
+{
+    [TestClass()]
+    public class EjerciciosTests
+    {
+        //Consigna 1
+        [TestMethod()]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void DivisionTest1()
+        {
+            //Arrange
+            int num1 = 20;
+            int num2 = 0;
+
+            //Act
+            Ejercicios.division(num1, num2);
+
+        }
+
+
+        //Consigna 2
+        [TestMethod()]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void DivisionTest2()
+        {
+            //Arrange
+            int num1 = 20;
+
+            //Act
+            Ejercicios.Operacion(num1, null);
+
+        }
+    }
+}
